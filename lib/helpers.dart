d989eed5225767ab8e099fb0ca7d5e3d13141d22
music-soul1-1 <72669184+music-soul1-1@users.noreply.github.com>
2024-08-11 19:02:15 +0300

    feat(ui): add info dialogs for groups, teachers and auditoy in lesson info dialog

--- lib/helpers.dart
+++ lib/helpers.dart
@@ -20,7 +20,10 @@ import 'package:flutter/services.dart';
 import 'package:flutter_localization/flutter_localization.dart';
 import 'package:intl/intl.dart';
 import 'package:nure_timetable/locales/locales.dart';
+import 'package:nure_timetable/models/auditory.dart';
+import 'package:nure_timetable/models/group.dart';
 import 'package:nure_timetable/models/lesson.dart';
+import 'package:nure_timetable/models/teacher.dart';
 
 
 final FlutterLocalization localization = FlutterLocalization.instance;
@@ -41,6 +44,41 @@ void copyLessonDetails(BuildContext context, Lesson lesson) {
       "${AppLocale.pairNumber.getString(context)}: ${lesson.numberPair}" "\n"
       "${AppLocale.groups.getString(context)}: ${lesson.groups.map((group) => "${group.name}(${group.faculty.shortName})").join(", ")}" "\n"
       "${AppLocale.auditory.getString(context)}: ${lesson.auditory.name}, ${AppLocale.floor.getString(context).toLowerCase()} - ${lesson.auditory.floor}"
+    ),
+  );
+}
+
+void copyGroupDetails(BuildContext context, Group group) {
+  Clipboard.setData(
+    ClipboardData(
+      text: "${group.name}""\n"
+      "ID: ${group.id}""\n"
+      "${AppLocale.faculty.getString(context)}: ${group.faculty.fullName}""\n"
+      "${AppLocale.direction.getString(context)}: ${group.direction.fullName}"
+    ),
+  );
+}
+
+void copyTeacherDetails(BuildContext context, Teacher teacher) {
+  Clipboard.setData(
+    ClipboardData(
+      text: "${teacher.fullName}""\n"
+      "ID: ${teacher.id}""\n"
+      "${AppLocale.faculty.getString(context)}: ${teacher.faculty.fullName}""\n"
+      "${AppLocale.department.getString(context)}: ${teacher.department.fullName}"
+    ),
+  );
+}
+
+void copyAuditoryDetails(BuildContext context, Auditory auditory) {
+  Clipboard.setData(
+    ClipboardData(
+      text: "${auditory.name}""\n"
+      "ID: ${auditory.id}""\n"
+      "${AppLocale.building.getString(context)}: ${auditory.building.fullName}""\n"
+      "${AppLocale.floor.getString(context)}: ${auditory.floor}""\n"
+      "${AppLocale.auditoryTypes.getString(context)}: ${auditory.auditoryTypes.map((t) => t.name).join(", ")}""\n"
+      "${AppLocale.hasPower.getString(context)}: ${auditory.hasPower ? AppLocale.yes.getString(context) : AppLocale.no.getString(context)}"
     ),
   );
 }
